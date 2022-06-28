1fa71530e8d789a68a5326d139509c3014036a43
Bank-Builder <andrew.turpin@cyber-mint.com>
2022-06-28 12:26:33 +0200

    Fix for using resolvectl with systemd

--- myip.sh
+++ myip.sh
@@ -5,20 +5,20 @@
 # Copyright (c) 2020 bank-builder                   #
 #####################################################
 
-DEV=$(ip -o -f inet addr show | awk '/scope global/ {print $2}')
+DEV=$(ip -o -f inet addr show | awk '/scope global/ {print $2}' | head -n1 )
 IP=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
 MASK=$(ip -o -f inet addr show | awk '/scope global/ {print $6}')
 GATEWAY=$(ip route show 0.0.0.0/0 dev $DEV | cut -d\  -f3)
 PUBLIC_IP=$(curl ifconfig.me 2>/dev/null )
-ACTIVE_DNS=$(systemd-resolve --status $DEV | grep "Current DNS" | awk '/: / {print $4}' )
+ACTIVE_DNS=$(resolvectl | grep "DNS Servers" |  awk '/: / {print $3" "$4}')
 
 echo "-----------------------------------------"
 echo " NETWORK INFORMATION ver 1.0             "
 echo "-----------------------------------------"
 echo ""
-echo "        IP Address: "$IP
+echo "      IP Addresses: "$IP
 echo "   Network Adapter: "$DEV
-echo "     Wildcard Mask: "$MASK
+echo "    Wildcard Masks: "$MASK
 echo "           Gateway: "$GATEWAY
 echo "         Public IP: "$PUBLIC_IP
 echo "        Active DNS: "$ACTIVE_DNS
